0d05a59b050702642ac70342842ba9dbaacedcbf
treverbrice <treverbrice@gmail.com>
2022-04-19 14:47:10 -0700

    Add createGameBoard function
    createGameBoard makes a square grid of divs with a given size

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-const gameBoard = document.querySelector("#gameBoard");
+// create the gameBoard
+function createGameBoard(size) {
+  const gameBoard = document.querySelector("#gameBoard");
+  for(let row = 1; row <= size; row++) {
+    for(let col = 1; col <= size; col++) {
+      const gamePiece = document.createElement("div");
+      gameBoard.appendChild(gamePiece);
+    }
+  }
+}
+  // create each div
+  // attach listeners and classes
+
+// attach listeners to buttons and sliders
+
+//color modes
+
+//listener functions
+
+createGameBoard(4);
